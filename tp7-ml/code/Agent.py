3d182cbbb6475379dbf245d00629a9556ab36c2a
Mrtc101 <martincogo4@gmail.com>
2022-10-21 10:43:11 -0300

    "adding files"

---
+++ tp7-ml/code/Agent.py
@@ -0,0 +1,45 @@
+from random import randint,choices, random
+import math
+import pandas as pd
+from symbol import expr_stmt
+import treelib as t
+
+class Agent:
+    #sa
+    def plularityValue(dataset : pd.DataFrame):
+        outputValues = pd.unique(set[""]);
+        common = outputValues[0]
+        for value in outputValues:
+            #contar la cantidad de veces que aparece el valor
+            print()
+        return mostCommon
+    
+    #Applies 
+    def Importance(arg,dataset):
+        print()
+    #
+    def maxArgument(dataset):
+        print()
+    #
+    def difference(setA,setB):
+        
+        return setC
+   
+   
+    """
+    def decisionTreeLearning(self,examples : pd.DataFrame,attributes : pd.DataFrame.,parentExample : pd.DataFrame) -> t.Tree:
+        if(len(examples.index) == 0):
+            return plularityValue(parentExample)
+        elif(len(examples.index) == 0):
+            return True
+        elif (len(attributes) == 0 and) :
+            return
+        else:
+            a = maxArgument(attributes)
+            A = Importance(a,examples)
+            tree = newTree(A)
+            for vk in A:
+                newExamples =  examples
+                subtree = decisionTreeLearning(newExamples,difference(attributes,A),examples)
+        return tree
+    """
---
+++ tp7-ml/code/main.py
@@ -0,0 +1,11 @@
+import Results as rs
+import time
+
+if __name__== "__main__":
+    #params
+    agent = Agent()
+    start = time.time()
+    agent.
+    end = time.time()
+    print(end-start)    
+    
