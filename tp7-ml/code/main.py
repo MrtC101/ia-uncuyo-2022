e80abf087b5837b18184c04887f01ca8fa659140
Mrtc101 <martincogo4@gmail.com>
2022-10-23 22:29:56 -0300

    Implementación del Tree

--- tp7-ml/code/main.py
+++ tp7-ml/code/main.py
@@ -1,11 +1,17 @@
-import Results as rs
-import time
+import pandas as pd
+from Agent import Agent
 
 if __name__== "__main__":
-    #params
-    agent = Agent()
-    start = time.time()
-    agent.
-    end = time.time()
-    print(end-start)    
-    
+    path = ".\\data\\tennis.csv"
+    dataset = pd.read_csv(path)
+    outputLabel = dataset.columns[len(dataset.columns)-1]
+    ag = Agent(outputLabel)
+    #divide
+    trainSet = dataset.sample(frac=0.8,random_state=1)
+    print(trainSet)
+    testSet = dataset.drop(trainSet.index)
+    del testSet[outputLabel]
+    treeModel = ag.decisionTreeLearning(trainSet)
+    treeModel.show()
+    ag.test(testSet,treeModel)
+    print(testSet)
